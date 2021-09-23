6964fc5575155e1319cd7c9ea27a16f53a3244d8
Fernando <Fernando32345@gmail.com>
2021-09-23 01:04:00 -0400

    Version 3 (Optimized Program Design)

--- Farkle.java
+++ Farkle.java
@@ -1,63 +1,75 @@
 import java.util.*;
-// 2 hour and a half
+// I spent approximately 16                                                                                    hour and a half
 
-public class Farkle{
-  public static void main(String[] args)
-  {
-  check_score();
-}
+public class Farkle
+{
+  //variables
+  public int dice;
+  public int check_1;
+  public int check_5;
+  public int turn_score = 0;
+  public int final_score = 0;
+  List<Integer> dicelist = new ArrayList<Integer>();
+
     //purpose: roll a dice
     //input: none
     //output: a random number between 1 and 6
     //assumption: number will be a positive and be between 1 and 6
-    public static int diceroll() {
-      int dice=(int)(Math.random()*6+1);
+    public int diceroll() {
+      dice=(int)(Math.random()*6+1);
       return dice;
     }
 
 
-    //purpose: check score based on dice rolled
-    //input: none
-    //output: total score for the turn
-    //assumption: score will be positive and a integer
 
-    public static void check_score() {
-      //variables
-      int dice;
-
-
-      //create a array with dice rolled 6 times
-      List<Integer> dicelist = new ArrayList<Integer>();
+    private List<Integer> rollDie()
+    {
       for(int i = 0;i < 6;i++){
         dicelist.add(diceroll());
       }
-      int [] scorelist;     //creating scoring array and adding 2 indexes
+      return dicelist;
+    }
+
+    private int Compute_TurnScore(List<Integer> dice)
+    {
+      int [] scorelist;
       scorelist =  new int[2];
       scorelist[0] = 0;
       scorelist[1] = 0;
 
-      for(int i = 0; i < dicelist.size(); i++) {
-        if(dicelist.get(i) ==  1){
+      for(int i = 0; i < dicelist.size(); i++) //if 1 is in the randomly rolled list then increment index pos. 0 by 1
+      {
+        if(dicelist.get(i) ==  1)
+        {
           scorelist[0] = scorelist[0] + 1;
-}
-
-        if(dicelist.get(i) ==  5){
-          scorelist[1] = scorelist[1] + 1;
-}
-
-
-
-
       }
 
-//test
-    System.out.println(scorelist[0]);
-    System.out.println(scorelist[1]);
-    for(int x = 0; x < dicelist.size(); x++){
-      System.out.println(dicelist.get(x));
-  }
+        if(dicelist.get(i) ==  5) //If 5 is in the randomly rolled list then increment index pos. 1 by 1
+        {
+          scorelist[1] = scorelist[1] + 1;
+      }
+    }
+
+    check_1=scorelist[0];
+    check_5= scorelist[1];
+    check_1 = check_1*100;
+    check_5 =check_5 *50;
+    turn_score = check_1 + check_5;
+
+    return turn_score;
   }
 
-
+public void play()
+{
+  turn_score = 0;
+  final_score = 0;
+  while (final_score < 10000)
+  {
+    List<Integer>dice = rollDie();
+    turn_score = Compute_TurnScore(dice);
+    final_score = final_score + turn_score;
+    System.out.println(final_score);
+  }
+}
 
 }
